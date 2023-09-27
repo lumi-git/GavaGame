a35cee6513fb0e7f6d2076c88713fe2d00baee63
ronan tremoureux <ronan.tremoureux@gmail.com>
2023-09-27 10:32:27 +0200

    update example for last lib version

--- src/defaultCube.java
+++ src/defaultCube.java
@@ -1,8 +1,8 @@
-import Gava.DrawRectComponent;
+import Gava.DrawableComponents.DrawRectComponent;
 import Gava.Game;
 import Gava.GameObject;
 import Gava.Vector2D;
-
+import Gava.Input;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
@@ -21,21 +21,36 @@ public class defaultCube extends GameObject {
     }
     @Override
     public void update(double dt) {
-        getTransform().setRotation(getTransform().getRotation()-0.01);
 
-        if(Game.getInstance().getKeyListener().isKeyPressed(KeyEvent.VK_Z)){
-            getTransform().getPosition().y -=speed;
+        smoothMove();
+    }
+
+    private void preciseMove(){
+        Vector2D mousePos = Input.getMousePosition();
+        getTransform().setPosition(mousePos);
+    }
+
+    private void smoothMove(){
+        Vector2D mousePos = Input.getMousePosition();
+        Vector2D direction = mousePos.subtract(getTransform().getPosition()).normalize();
+
+
+        if (Input.isMousePressed(1) ){
+            getTransform().Move(direction,speed);
         }
-        if(Game.getInstance().getKeyListener().isKeyPressed(KeyEvent.VK_S)){
+
+        if(Input.isKeyPressed(KeyEvent.VK_Z)){
+
+        }
+        if(Input.isKeyPressed(KeyEvent.VK_S)){
             getTransform().getPosition().y +=speed;
         }
-        if(Game.getInstance().getKeyListener().isKeyPressed(KeyEvent.VK_Q)){
+        if(Input.isKeyPressed(KeyEvent.VK_Q)){
             getTransform().getPosition().x -=speed;
         }
-        if(Game.getInstance().getKeyListener().isKeyPressed(KeyEvent.VK_D)){
+        if(Input.isKeyPressed(KeyEvent.VK_D)){
             getTransform().getPosition().x +=speed;
         }
-
     }
 
 }
